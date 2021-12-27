ec523d042d98926594ba85f76ef7d0d769d06a3c
lucasbecker-dev <lucas@lucasbecker.dev>
2021-12-27 01:05:03 -0500

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyA8Fi_mjPy1BVeEVDLUsZzhh71pvk5CTCQ",
+
+    authDomain: "fb-clone-e6a20.firebaseapp.com",
+
+    projectId: "fb-clone-e6a20",
+
+    storageBucket: "fb-clone-e6a20.appspot.com",
+
+    messagingSenderId: "53894357882",
+
+    appId: "1:53894357882:web:48e874976d9ba6afaf2cc3",
+
+    measurementId: "G-ZKWEV0K0FC"
+
+};
