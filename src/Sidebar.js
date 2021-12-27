ff8b797672ea9b593aec4b6c5d4486507e956615
lucasbecker-dev <lucas@lucasbecker.dev>
2021-12-27 03:10:48 -0500

    first pass

---
+++ src/Sidebar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import EmojiFlagsIcon from "@mui/icons-material/EmojiFlags";
+import PeopleIcon from "@mui/icons-material/People";
+import ChatIcon from "@mui/icons-material/Chat";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import ExpandMoreOutlined from "@mui/icons-material/ExpandMoreOutlined";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <SidebarRow src={"./images/favicon-32x32.png"} title={"Lucas Becker"} />
+      <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Info" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
+    </div>
+  );
+}
+
+export default Sidebar;
