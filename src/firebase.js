5b134861e7d89c1918769b59f5d4cab513fa1af3
lucasbecker-dev <lucas@lucasbecker.dev>
2021-12-27 03:09:40 -0500

    introduced some global formatting

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
 const firebaseConfig = {
+  apiKey: "AIzaSyA8Fi_mjPy1BVeEVDLUsZzhh71pvk5CTCQ",
 
-    apiKey: "AIzaSyA8Fi_mjPy1BVeEVDLUsZzhh71pvk5CTCQ",
+  authDomain: "fb-clone-e6a20.firebaseapp.com",
 
-    authDomain: "fb-clone-e6a20.firebaseapp.com",
+  projectId: "fb-clone-e6a20",
 
-    projectId: "fb-clone-e6a20",
+  storageBucket: "fb-clone-e6a20.appspot.com",
 
-    storageBucket: "fb-clone-e6a20.appspot.com",
+  messagingSenderId: "53894357882",
 
-    messagingSenderId: "53894357882",
+  appId: "1:53894357882:web:48e874976d9ba6afaf2cc3",
 
-    appId: "1:53894357882:web:48e874976d9ba6afaf2cc3",
-
-    measurementId: "G-ZKWEV0K0FC"
-
+  measurementId: "G-ZKWEV0K0FC",
 };
